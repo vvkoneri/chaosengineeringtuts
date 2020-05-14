5fb4ce46feba34d23bdfc8d118f0838d5ed8e03c
Venkatesh <vekoneri@deloitte.com.au>
2020-05-14 23:41:44 +0530

    Initial commit with the services updated

---
+++ ServiceB.groovy
@@ -0,0 +1,84 @@
+@Grapes([
+    @Grab('org.eclipse.jetty:jetty-server:9.4.3.v20170317'),
+    @Grab('org.eclipse.jetty:jetty-servlet:9.4.3.v20170317'),
+    @Grab('mysql:mysql-connector-java:8.0.17')
+])
+@GrabConfig(systemClassLoader = true)
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import groovy.sql.Sql
+import groovy.json.JsonSlurper
+
+class JettyServer {
+    Server server;
+ 
+    public void start() throws Exception {
+        server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8091);
+        server.addConnector(connector);
+        
+        ServletHandler servletHandler = new ServletHandler();
+        server.setHandler(servletHandler);
+
+        servletHandler.addServletWithMapping(ServiceBServlet.class, "/search");
+        
+        server.start()
+        
+    }
+}
+
+class ServiceBServlet extends HttpServlet {
+    
+    def username = 'root'
+    def password = 'password123'
+    def sql = Sql.newInstance("jdbc:mysql://localhost:3306/company",username,password,"com.mysql.jdbc.Driver")
+    
+    protected void doGet(
+      HttpServletRequest request, 
+      HttpServletResponse response)
+      throws ServletException, IOException {
+  
+        def department = request.getParameter("dept")
+        def lastname = request.getParameter("ln")
+        
+        response.setContentType("application/json");
+        
+        def employee = sql.rows("select * from employees where department_id = ${department} and last_name LIKE ${lastname}")
+      
+        def jsonBuilder = new groovy.json.JsonBuilder()
+        
+        jsonBuilder {
+            employees employee.collect {
+                [
+                    'firstname' : it.get('first_name'),
+                    'lastname' : it.get('last_name'),
+                    'department_name' : it.get('department_name'),
+                    'department_id' : it.get('department_id'),
+                    'age' : it.get('age')
+                    
+                ]
+            }
+        }
+        
+           
+        response.getWriter().println(jsonBuilder.toString())
+    }
+    
+   
+    
+       
+}
+
+JettyServer server = new JettyServer()
+server.start()
